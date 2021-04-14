124601ff79ef4601163f30a1be0741a7bdf5ffae
delta <dentadelta123@gmail.com>
2021-04-14 12:12:25 +1000

    Modified Config

--- Config.py
+++ Config.py
@@ -1,12 +1,11 @@
 class Config():
 
-	def __init__(self):
-		self.h = 8 			#Number of attention layers
-		self.N = 6			#Number of stacks
-		self.dmodel = 512	#Number of feature
-		self.dk = int(self.dmodel/self.h)
-		self.dv = int(self.dmodel/self.h)
-		self.dff = self.dk*self.h
+	self.h = 8 			#Number of attention layers
+	self.N = 6			#Number of stacks
+	self.dmodel = 512	#Number of feature
+	self.dk = int(self.dmodel/self.h)
+	self.dv = int(self.dmodel/self.h)
+	self.dff = self.dk*self.h
 
 
 		
