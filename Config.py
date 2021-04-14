eae5d808be9de9d74ad835e31fb81744e869491f
delta <dentadelta123@gmail.com>
2021-04-14 12:23:24 +1000

    modified config file

--- Config.py
+++ Config.py
@@ -1,11 +1,11 @@
-class Config():
-
-	self.h = 8 			#Number of attention layers
-	self.N = 6			#Number of stacks
-	self.dmodel = 512	#Number of feature
-	self.dk = int(self.dmodel/self.h)
-	self.dv = int(self.dmodel/self.h)
-	self.dff = self.dk*self.h
+class conf(object):
+	def __init__(self):
+		self.h = 8 			#Number of attention layers
+		self.N = 6			#Number of stacks
+		self.dmodel = 512	#Number of feature
+		self.dk = int(self.dmodel/self.h)
+		self.dv = int(self.dmodel/self.h)
+		self.dff = self.dk*self.h
 
 
 		
